59702a6e040127f535e939167b5b4b8f31cc8ac8
abdul ahad <abdul_ahad1996@live.com>
2019-05-31 06:58:21 +0500

    setting up ui

---
+++ AdvancedCoreData/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AdvancedCoreData
+//
+//  Created by prog on 5/31/19.
+//  Copyright © 2019 prog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
